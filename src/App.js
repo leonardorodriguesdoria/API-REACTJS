ef554b07657b6ed6d462144d388f39707f881f38
Leonardo <leordoria2016@gmail.com>
2021-02-23 20:27:22 -0300

    REACT NUTRI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './style.css';
+//https://sujeitoprogramador.com/rn-api/?api=posts
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            nutri:[]
+        }
+    }
+
+    componentDidMount(){
+        let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+        fetch(url)
+        .then((r)=> r.json())
+        .then((json)=>{
+            let state = this.state;
+            state.nutri = json;
+            this.setState(state);
+        })
+
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <header>
+                    <stron>REACT NUTRI</stron>
+                </header>
+
+                {this.state.nutri.map((item)=>{
+                    return(
+                        <article key={item.id} className="post">
+                            <strong className="titulo"> {item.titulo} </strong>
+                            <img src = {item.capa} className="capa"/>
+                            <p className="subtitulo">{item.subtitulo}</p>
+                            <a className="botao" href="#">VEJA MAIS</a>
+                        </article>
+                    );
+                })}
+
+            </div>
+        ); 
+
+    }
 }
 
 export default App;
